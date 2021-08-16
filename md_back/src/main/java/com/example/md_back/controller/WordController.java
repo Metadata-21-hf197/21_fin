5b9cc6dca2a5c21ce2a404c2e8fc5b754ecebbe9
uoise <yw823x@daum.net>
2021-08-16 17:27:30 +0900

    [update] WordContoller 경로 수정

--- md_back/src/main/java/com/example/md_back/controller/WordController.java
+++ md_back/src/main/java/com/example/md_back/controller/WordController.java
@@ -12,15 +12,17 @@ public class WordController {
     @Autowired
     private WordService wordService;
 
+    @GetMapping("/word/insert")
+    @ResponseBody
     public String wordForm(){
-        return null;
+        return "word/form";
     }
 
     /** 단어 추가 시 권한 확인, 세션의 유저 정보 받아서 서비스로 리턴도 해줘야함
      * @param
      * @return
      */
-    @PostMapping("/word") // /word/insert 일 필요가 있는지?
+    @PostMapping("/word/insert") // /word/insert 일 필요가 있는지?
     public Map<String, Object> insert(){ // 세션의 유저 정보 받아 옴
         // word 객체 받아옴
         wordService.insertWord(null); // 유저정보 추가 할것
@@ -29,10 +31,11 @@ public class WordController {
     }
 
     /** 단어 수정 시 권한 확인, 세션의 유저 정보 받아서 서비스로 리턴도 해줘야함
+     * 삭제도 이 곳에서 처리, deleteStatus = True로 update
      * @param wordId, word, principal
      * @return
      */
-    @PutMapping("/word/{wordId}") // /word/{wordId}로 수정
+    @PutMapping("/word/update/{wordId}") // /word/{wordId}로 수정
     public Map<String, Object> update(@PathVariable int wordId){ // 세션의 유저 정보 받아 옴
         // word 객체 받아옴
         wordService.updateWord(wordId,null); // 유저정보 추가 할것
@@ -41,10 +44,11 @@ public class WordController {
     }
 
     /** 단어 삭제 시 권한 확인
+     * DB 에서 단어 삭제 시 사용
      * @param wordId
      * @return
      */
-    @DeleteMapping("/word/{wordId}")
+    @DeleteMapping("/word/delete/{wordId}")
     public Map<String, Object> delete(@PathVariable int wordId){
         wordService.deleteWord(wordId);
         // httpStatus return
