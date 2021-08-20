0330c7b018dc2176a717f9cc1490dede3ec0e582
innjie <innjie@naver.com>
2021-08-20 17:31:31 +0900

    [add] mybatis user test files

---
+++ md_back/src/main/java/com/example/md_back/mappers/UserMapper.java
@@ -0,0 +1,9 @@
+package com.example.md_back.mappers;
+
+import com.example.md_back.model.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    public User getUser(String memberName);
+}
--- md_back/src/main/java/com/example/md_back/user/UserTest.java
+++ md_back/src/main/java/com/example/md_back/user/UserTest.java
@@ -3,15 +3,25 @@ package com.example.md_back.user;
 import com.example.md_back.model.User;
 import com.example.md_back.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserTest {
 
+//    @Autowired
+//    private UserRepository userRepository;
+
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
+
+    @GetMapping("/user/login/{memberName}")
+    @ResponseBody
+    public String getUserTest(@PathVariable String memberName) {
+        User user = userService.getUser(memberName);
+
+        return user.getMemberName() + " / " + user.getPassword();
+
+    }
 
     @GetMapping("/user/test")
     public void userTest(){
@@ -25,7 +35,7 @@ public class UserTest {
                 .build();
 
         System.out.println(user);
-        userRepository.save(user);
+//        userRepository.save(user);
 
     }
 }
