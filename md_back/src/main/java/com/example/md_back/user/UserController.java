e41b5eb3b61df4cf051fd60269f294e103e61e24
innjie <innjie@naver.com>
2021-08-23 21:51:26 +0900

    [add] security files

--- md_back/src/main/java/com/example/md_back/user/UserController.java
+++ md_back/src/main/java/com/example/md_back/user/UserController.java
@@ -36,7 +36,9 @@ public class UserController {
         //compare DB
         User checkUser = userService.checkUser(user);
         HttpSession userSession = null;
+        Map<String, Object> map = new HashMap<String, Object>();
         if (checkUser == null) { //not correct
+            map.put("errormsg", "No User");
         } else { //correct
             System.out.println("login Success");
             userSession = request.getSession();
@@ -44,7 +46,7 @@ public class UserController {
 
 
         }
-        Map<String, Object> map = new HashMap<String, Object>();
+
         map.put("user", userSession);
 
         return map;
