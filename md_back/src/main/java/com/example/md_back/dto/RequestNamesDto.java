d43d74122e0660f0471d23336715ea67fdf6c94a
uoise <yw823x@daum.net>
2021-08-27 19:10:03 +0900

    [add] RequestNamesDto

---
+++ md_back/src/main/java/com/example/md_back/dto/RequestNamesDto.java
@@ -0,0 +1,11 @@
+package com.example.md_back.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RequestNamesDto {
+    private String shortName;
+    private String engName;
+    private String korName;
+    private boolean banWord;
+}
