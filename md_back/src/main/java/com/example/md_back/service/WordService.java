e7eaa9cc28b699c3b541259529b297272ead29fe
uoise <yw823x@daum.net>
2021-11-16 15:16:52 +0900

    [update] collection mapping

--- md_back/src/main/java/com/example/md_back/service/WordService.java
+++ md_back/src/main/java/com/example/md_back/service/WordService.java
@@ -53,7 +53,6 @@ public class WordService {
         if (word == null) throw new IllegalArgumentException("결재 생성 실패 : 단어를 찾을 수 없습니다.");
         else if (word.isDeleteStatus()) throw new IllegalArgumentException("결재 생성 실패 : 이미 삭제된 단어입니다.");
 
-
         Approval approval = new Approval();
         approval.setCreateUser(user);
         approval.setTargetId(targetId);
